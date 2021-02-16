208cc32860d6390ca908ac22a6c3352e963257cc
KuanWei <KuanWei@DESKTOP-8R38C1M>
2021-02-16 21:12:03 +0800

    java8_1z0809 init

--- TestApp3/src/java8_1z0809/Sample2.java
+++ TestApp3/src/java8_1z0809/Sample2.java
@@ -1,5 +1,10 @@
 package java8_1z0809;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Sample2 {
 	public static void main(String[] args) throws Exception {
 		Practice.get("src\\java8_1z0809").run();
@@ -14,6 +19,18 @@ class Pa {
 
 class P2 {
 	public static void main(String[] args) {
-		S.o.pln("test");
+		String source = "";
+		String target = "";
+		byte[] bs = new byte[128];
+		try (FileInputStream fis = new FileInputStream(source); FileOutputStream fos = new FileOutputStream(target)) {
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
